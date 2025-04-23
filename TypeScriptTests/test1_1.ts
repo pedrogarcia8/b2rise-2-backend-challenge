d8745cd83ba3a135d0f50c29592206851553dd51
pedrogarcia8 <pedro.jose.garcia8@gmail.com>
2025-04-23 11:27:35 -0300

    typeScriptTest-1.1: Add test answer

---
+++ TypeScriptTests/test1_1.ts
@@ -0,0 +1,16 @@
+//Manipulação de Tipos e Generics
+
+const extractValues = <T extends object, K extends keyof T>(array: T[], key: K): T[K][] => {
+  try {
+    return array.reduce((newArray: T[K][], object: T) => {
+      if (key in object) 
+        newArray.push(object[key]);
+      
+      return newArray;
+    }
+    , [] as T[K][]);
+  } catch (error) {
+    console.log('Error in extractValues function: ', error);
+    return [] as T[K][];
+  }
+}
